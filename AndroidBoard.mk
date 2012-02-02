36be84368046dc78cae6092de629245461ae0835
Kriz <kriz2000@wp.pl>
2012-02-02 18:26:33 +0100

    small fix

--- AndroidBoard.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
-
-
-# include the non-open-source counterpart to this file
--include vendor/lg/swift/AndroidBoardVendor.mk
